12ff34aa9308c20229b655993b964d905736eaf3
norujam@gmail.com <!abc76925>
2018-02-13 13:48:43 +0900

    API 호출 전 메세지 확인(호출여부)

--- chatbot/views.py
+++ chatbot/views.py
@@ -3,7 +3,11 @@ from django.http import JsonResponse
 from chatbot.chatLogObjectMap import ChatLogObjectMap
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.clickjacking import xframe_options_exempt
-import logging, json, requests, configparser
+from chatbot import doNotCallApi as DoNotCallApi
+import logging
+import json
+import requests
+import configparser
 
 logger = logging.getLogger("django")
 
@@ -13,16 +17,17 @@ config.read('config.properties')
 
 def index(request):
     logger.debug("vue index")
-    return render(request, 'chatbot/index.html', )
+    return render(request, 'chatbot/index.html')
 
 
 def message(request):
     message_value = request.POST['message']
-    if config['setSite']['dialogLoc'] == "outer":
+    result = DoNotCallApi.check_message(message_value)
+    if result is None and config['setSite']['dialogLoc'] == "outer":
         url = config['setSite']['dialogUrl']
         payload = {"message": message_value}
         result = json.loads(requests.post(url, data=payload).text)
-    else:
+    elif result is None:
         api_call_module = __import__(config['setSite']['apiCallModule'], fromlist=["detect_intent_texts"])
         result = api_call_module.detect_intent_texts([message_value])
         ChatLogObjectMap.insert_log(result)
