d299eace8f37dbd8f8f4af3fa5d14fccd8af1e10
jinseok <norujam@gmail.com>
2018-03-08 21:53:31 +0900

    테스트 용 서비스 추가

--- service/urls.py
+++ service/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('countryList/', views.country_list, name='countryList'),
+    path('studyList/', views.study_list, name='countryList'),
 ]
--- service/views.py
+++ service/views.py
@@ -11,11 +11,24 @@ config.read('config.properties')
 
 @csrf_exempt
 def country_list(request):
-    param_value = request.POST['paramValue']
-    logger.debug("param="+param_value)
+    country = request.POST['country']
     result = dict()
-    result["data"] = {}
-    if param_value in ['나라']:
-        result["data"]["result"] = "미국, 영국, 일본"
+    if country in ['나라']:
+        result["result"] = "미국, 영국, 일본"
+    elif country in ['유럽']:
+        result["result"] = "영국"
+
+    return JsonResponse(result)
+
+
+@csrf_exempt
+def study_list(request):
+    country = request.POST['country']
+    kind = request.POST['kind']
+    logger.debug("country="+country)
+    logger.debug("kind="+kind)
+    result = dict()
+    if country in ['미국'] and kind in ['유학']:
+        result["result"] = "20개가 있습니다."
 
     return JsonResponse(result)
